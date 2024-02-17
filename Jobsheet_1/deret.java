ac8894a09acf8c5b41f0c6a06712a10ae8316b5f
Raudhil <raudhilfirdausn@gmail.com>
2024-02-17 21:21:52 +0700

    Deret

--- Jobsheet_1/deret.java
+++ Jobsheet_1/deret.java
@@ -1,5 +1,26 @@
 package Jobsheet_1;
 
+import java.util.Scanner;
+
 public class deret {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM: ");
+        String nim = sc.next();
+
+        int n = Integer.parseInt(nim.substring(8));
+
+        for(int i = 1; i <= n; i++){
+            if (i == 6 || i == 10) {
+                continue;
+            } else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } 
+            else{
+                System.out.print("* ");
+            }
+        }
+    }
 }

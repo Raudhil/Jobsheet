ecb294494c3c41c1a47cae359a3c9b8dfb8e9df8
Raudhil <raudhilfirdausn@gmail.com>
2024-02-23 14:50:50 +0700

    Latihan Praktikum No.1

--- Jobsheet_2/buku22.java
+++ Jobsheet_2/buku22.java
@@ -3,14 +3,39 @@ package Jobsheet_2;
 public class buku22 {
     
         String judul,pengarang;
-        int halaman,stok,harga;
+        int halaman,stok,harga,hargaTotal;
+        double totalDiskon,diskon;
 
+        
         void tampilInformasi(){
             System.out.println("Judul: " + judul);
             System.out.println("Pengarang: " + pengarang);
             System.out.println("Jumlah halaman: " + halaman);
             System.out.println("Sisa stok: " + stok);
             System.out.println("Harga: Rp " + harga);
+        }
+
+        void hitungHargaTotal(int hrg, int trjl){
+            hargaTotal = harga * trjl;
+        }   
+
+        void hitungDiskon(){
+            if (hargaTotal > 150000) {
+                System.out.println("Selamat anda mendapatkan diskon sebesar 12%");
+                diskon = 0.12;
+            } else if (hargaTotal >= 75000 && hargaTotal <= 150000) {
+                System.out.println("Selamat anda mendapatkan diskon sebesar 5%");
+                diskon = 0.5;
+            } else{
+                System.out.println("Anda tidak mendapatkan diskon.");
+            }
+        }
+
+        void hitungHargaBayar(){
+            totalDiskon = hargaTotal * diskon;
+            System.out.println("Diskon yang didapat: " + totalDiskon);
+            hargaTotal -= totalDiskon;
+            System.out.println("Total Harga yang didapat: " + hargaTotal);
         }
 
         void terjual(int jml){
--- Jobsheet_2/bukuMain.java
+++ Jobsheet_2/bukuMain.java
@@ -10,16 +10,19 @@ public class bukuMain {
         buku1.stok = 13;
         buku1.harga = 71000;
 
-        buku1.tampilInformasi();
+        // buku1.tampilInformasi();
         buku1.terjual(5);
         buku1.gantiHarga(60000);
+        buku1.hitungHargaTotal(60000, 5);
+        buku1.hitungDiskon();
         buku1.tampilInformasi();
+        buku1.hitungHargaBayar();
 
-        buku22 buku2 = new buku22("Self Reward","Maheera Ayesha",160,29,59000);
-        buku2.terjual(11);
-        buku2.tampilInformasi();
+        // buku22 buku2 = new buku22("Self Reward","Maheera Ayesha",160,29,59000);
+        // buku2.terjual(11);
+        // buku2.tampilInformasi();
 
-        buku22 bukuRaul = new buku22("Si Belalang","Raudhil",110,5,2000000);
+        // buku22 bukuRaul = new buku22("Si Belalang","Raudhil",110,5,2000000);
 
 
 

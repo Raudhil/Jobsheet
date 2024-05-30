17e3e1e2517f0050c7da1cb22789d92943348569
Raudhil <raudhilfirdausn@gmail.com>
2024-05-30 18:31:12 +0700

    Jobsheet 11

---
+++ Jobsheet_11/node22.java
@@ -0,0 +1,17 @@
+package Jobsheet_11;
+
+public class node22 {
+    int data;
+    node22 left;
+    node22 right;
+
+    public node22(){
+
+    }
+
+    public node22(int data){
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
